dcb65ba052831d9522860d0ecb6af186c8ca246e
Emma Adams <23emmaadams@gmail.com>
2025-03-26 15:17:25 -0400

    updated routes and started basic layout

---
+++ resources/views/index.blade.php
@@ -0,0 +1,23 @@
+@extends('layouts.app')
+
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>ESSY Database</title>
+
+        <link rel="preconnect" href="https://fonts.bunny.net">
+        <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
+
+        @if (file_exists(public_path('build/manifest.json')) || file_exists(public_path('hot')))
+            @vite(['resources/css/app.css', 'resources/js/app.js'])
+        @else
+            <style></style>
+        @endif
+    </head>
+    <body>
+        
+    </body>
+</html>
